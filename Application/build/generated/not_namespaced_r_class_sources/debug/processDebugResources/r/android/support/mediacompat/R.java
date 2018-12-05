457d49089dff555a65d1e333aee88709ade4342f
Tyler Cowls <tcowls@ucla.edu>
2018-12-04 19:44:34 -0800

    updated margin constraints on blackjack so hit/stay/yes/no buttons are evenly spaced and play again text readable

--- Application/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/mediacompat/R.java
+++ Application/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/mediacompat/R.java
@@ -30,13 +30,13 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f040042;
-        public static final int notification_icon_bg_color = 0x7f040043;
-        public static final int notification_material_background_media_default_color = 0x7f040044;
-        public static final int primary_text_default_material_dark = 0x7f040049;
-        public static final int ripple_material_light = 0x7f04004e;
-        public static final int secondary_text_default_material_dark = 0x7f04004f;
-        public static final int secondary_text_default_material_light = 0x7f040050;
+        public static final int notification_action_color_filter = 0x7f040043;
+        public static final int notification_icon_bg_color = 0x7f040044;
+        public static final int notification_material_background_media_default_color = 0x7f040045;
+        public static final int primary_text_default_material_dark = 0x7f04004a;
+        public static final int ripple_material_light = 0x7f040050;
+        public static final int secondary_text_default_material_dark = 0x7f040051;
+        public static final int secondary_text_default_material_light = 0x7f040052;
     }
     public static final class dimen {
         private dimen() {}
